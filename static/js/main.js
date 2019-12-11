282013f565453846b650324f21bcaee5c4338625
Sophie <szaniszlozs7@gmail.com>
2019-12-11 14:32:14 +0100

    changed long html to javascript

--- static/js/main.js
+++ static/js/main.js
@@ -7,4 +7,33 @@ function getDirections() {
     ]
 }
 
+function setGameField() {
 
+    for (let i = 0; i < 10; i++) {
+        let row = document.createElement('tr');
+        row.setAttribute('id', 'row' + i.toString());
+        for (let j = 0; j < 10; j++) {
+            let cell = document.createElement('td');
+            cell.setAttribute('id', j.toString());
+            row.appendChild(cell);
+        }
+        document.querySelector('.game-field').appendChild(row);
+    }
+}
+
+function getWallandField() {
+    let game = [[0, 2, 5],[],[],[],[],[3,8,9],[],[],[],[]];
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (game[i].includes(j) ? document.querySelector('#row' + i).children[j].setAttribute('class', 'cell-wall') :
+                document.querySelector('#row' + 1).children[j].setAttribute('class', 'cell');
+        }
+    }
+}
+
+function main() {
+    setGameField();
+    getWallandField();
+}
+
+main()
